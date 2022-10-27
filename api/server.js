063c26b01dd144c092abe079a2d1386a2efe6a80
Jhevon Tulloch <Mrjtullo@gmail.com>
2022-10-27 01:17:39 -0400

    created js sever

--- api/server.js
+++ api/server.js
@@ -1,18 +1,28 @@
-const express =require('express');
-const mongoose =require('mongoose');
-const cors =require('cors');
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require('path');
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-const port = process.env.PORT || 3001
-const password = process.env.PASSWORD
+const port = process.env.PORT || 3001;
+const password = process.env.PASSWORD;
 
-mongoose.connect(`mongodb+srv://testing:${password}@cluster0.aldzgns.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://testing:${password}@cluster0.aldzgns.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("connectd to DB"))
+  .catch(console.error);
 
 
 
-app.listen(port, ()=>{console.log(`now listening on port: ${port}`)} )
+
+
+
+
+app.listen(port, () => {
+  console.log(`now listening on port: ${port}`);
+});
