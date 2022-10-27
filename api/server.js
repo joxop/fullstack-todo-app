dc442aa35642e78b89ac1e099f4136b2da1ac412
Jhevon Tulloch <Mrjtullo@gmail.com>
2022-10-26 23:47:33 -0400

    setting up work space

--- api/server.js
+++ api/server.js
@@ -9,6 +9,9 @@ app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT || 3001
+const password = process.env.PASSWORD
+
+mongoose.connect(`mongodb+srv://testing:${password}@cluster0.aldzgns.mongodb.net/?retryWrites=true&w=majority`)
 
 
 
